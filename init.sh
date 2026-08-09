8b3a0314de9bdd29ce29629ea6d0f1b3b729b859
iloElias <murilo7456@gmail.com>
2026-08-09 19:56:51 -0300

    refactor: update init script to dynamically create Docker networks from docker-compose configuration

--- init.sh
+++ init.sh
@@ -2,11 +2,18 @@
 
 set -e
 
-NETWORK_NAME="global-network"
+# Stores an array of networks contained on the docker-compose.yml file.
+NETWORKS=($(docker compose config --networks | awk '{print $1}'))
 
-if ! docker network inspect "${NETWORK_NAME}" >/dev/null 2>&1; then
-	echo "Creating Docker network: ${NETWORK_NAME}"
-	docker network create --driver bridge --attachable "${NETWORK_NAME}"
-else
-	echo "Docker network already exists: ${NETWORK_NAME}"
-fi
+for network in "${NETWORKS[@]}"; do
+	# Check if the network exists
+	if ! docker network inspect "$network" >/dev/null 2>&1; then
+		echo "Creating network: $network"
+		docker network create --driver bridge --attachable "$network"
+	else
+		echo "Network already exists: $network"
+	fi
+done
+
+# Starts the docker-compose services in detached mode
+docker compose up -d
